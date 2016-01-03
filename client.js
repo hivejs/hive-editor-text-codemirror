e7c4f6c4c6bd032d96ff92962dfb96a8db9ae8fb
Marcel Klehr <mklehr@gmx.net>
2016-01-03 18:12:31 +0100

    Refactor to work with redux-based ui

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ function setup(plugin, imports, register) {
   var editor = imports.editor
 
   editor.registerEditor('CodeMirror', 'text', 'An extensible and performant code editor'
-  , function*(editorEl) {
+  , function(editorEl) {
     var cm = CodeMirror(function(el) {
       editorEl.appendChild(el)
 
@@ -17,11 +17,6 @@ function setup(plugin, imports, register) {
     })
 
     editorEl.style['height'] = '100%'
-    document.body.style['position'] = 'absolute'
-    document.body.style['bottom'] =
-    document.body.style['top'] =
-    document.body.style['left'] =
-    document.body.style['right'] = '0'
 
     return bindCodemirror(cm)
   })
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var path = require('path')
   , textOT = require('ot-text').type
 
 module.exports = setup
-module.exports.consumes = ['assets', 'ot']
+module.exports.consumes = ['ui', 'ot']
 
 function setup(plugin, imports, register) {
-  var assets = imports.assets
+  var ui = imports.ui
   var ot = imports.ot
 
-  assets.registerModule(path.join(__dirname, 'client.js'))
-  assets.registerStylesheet(path.join(__dirname, 'static/codemirror.css'))
+  ui.registerModule(path.join(__dirname, 'client.js'))
+  ui.registerStylesheet(path.join(__dirname, 'static/codemirror.css'))
 
   ot.registerOTType('text', textOT)
 
