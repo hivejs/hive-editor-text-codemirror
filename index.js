112ebee76ba6c6542b12c9ed352439f2c8871ba5
Marcel Klehr <mklehr@gmx.net>
2016-01-17 23:58:52 +0100

    Implement import

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var path = require('path')
   , fs = require('fs')
 
 module.exports = setup
-module.exports.consumes = ['ui', 'ot', 'importexport']
+module.exports.consumes = ['ui', 'ot', 'importexport', 'orm', 'sync']
 
 function setup(plugin, imports, register) {
   var ui = imports.ui
     , ot = imports.ot
     , importexport = imports.importexport
+    , orm = imports.orm
+    , sync = imports.sync
 
   ui.registerModule(path.join(__dirname, 'client.js'))
   ui.registerStylesheet(path.join(__dirname, 'static/codemirror.css'))
@@ -35,6 +37,28 @@ function setup(plugin, imports, register) {
   importexport.registerExportProvider('text/plain', 'text/plain'
   , function* (document, snapshot) {
     return snapshot.contents
+  })
+
+  importexport.registerImportProvider('text/plain', 'text/plain'
+  , function* (document, user, data) {
+    var snapshot = yield orm.collections.snapshot
+      .findOne({id: document.latestSnapshot})
+
+    // Append data to the current contents
+    var changes
+    if(snapshot.contents.length)
+      changes = [snapshot.contents.length, '\n'+data.toString('utf8')]
+    else
+      changes = [data.toString('utf8')]]
+
+    var gulfDoc = yield sync.getDocument(document.id)
+    yield function(cb) {
+      gulfDoc.receiveEdit(JSON.stringify({
+        cs: JSON.stringify(changes)
+      , parent: snapshot.id
+      , user: user
+      }), null, cb)
+    }
   })
 
   register()
