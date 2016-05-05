7df02c42307f8b4a032a46b7bf41763727c678ab
Marcel Klehr <mklehr@gmx.net>
2016-05-05 13:12:27 +0200

    [legal] Fix license headers

--- client.js
+++ client.js
@@ -1,3 +1,19 @@
+/**
+ * hive.js
+ * Copyright (C) 2013-2016 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License version 2
+ * as published by the Mozilla Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License
+ * along with this program.  If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
+ */
 var CodeMirror = require('codemirror')
   , bindCodemirror = require('gulf-codemirror')
   , vdom = require('virtual-dom')
